fcb5785b825d3b8d0c30393b5d9fd6c174b128f7
Kavita Kalaichelvan <kkavita92@gmail.com>
2017-04-12 03:23:14 +0800

    Add solution

--- bracket-duplicates.rb
+++ bracket-duplicates.rb
@@ -6,3 +6,9 @@ For example, the input "aaaabbcdefffffffg" should return "aa[aa]bbcdeff[fffff]g"
 Please also ensure that the input is a string, and return "Please enter a valid string" if it is not.
 
 =end
+
+def string_parse string
+  string.is_a?(String) ? string.gsub(/(.)\1(\1+)/,'\1\1[\2]') : "Please enter a valid string"
+  #(.)\1 assures that same consecutive characters is being searched for 
+  #\1 is a backreference to the first capture group (what's in brackets) 
+end
